6eeb8c8742356e8398bce88aa229d5bb4a3975b3
Bram Adams <3282661+bramses@users.noreply.github.com>
2026-02-05 20:39:27 -0500

    Initial Commit

---
+++ CountingWorms/CountingWormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountingWormsApp.swift
+//  CountingWorms
+//
+//  Created by Bram Adams on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CountingWormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountingWormsTests/CountingWormsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountingWormsTests.swift
+//  CountingWormsTests
+//
+//  Created by Bram Adams on 2/5/26.
+//
+
+import Testing
+@testable import CountingWorms
+
+struct CountingWormsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
